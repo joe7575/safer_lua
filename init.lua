16d3c0cedfcc34836e3ab6459ce4fa49fde8ee89
Joachim Stolberg <joe.stolberg@gmx.de>
2020-05-10 18:47:29 +0200

    demo code added, test code removed

--- init.lua
+++ init.lua
@@ -20,4 +20,5 @@ safer_lua.version = 1.0
 dofile(minetest.get_modpath("safer_lua") .. "/data_struct.lua")
 dofile(minetest.get_modpath("safer_lua") .. "/scanner.lua")
 dofile(minetest.get_modpath("safer_lua") .. "/environ.lua")
-dofile(minetest.get_modpath("safer_lua") .. "/demo.lua")
+-- only for demo purposes
+--dofile(minetest.get_modpath("safer_lua") .. "/demo.lua")
